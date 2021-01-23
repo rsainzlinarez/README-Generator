6668d2981d7f54ba386026e8ee2ebd4dff170622
Ruben Sainz Linarez <rsainzlinarez@gmail.com>
2021-01-22 23:27:43 -0800

    Added functions to render license link and badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,27 +1,53 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+const licenses = {
+  MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+  Mozilla: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+  IBM: '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)',
+}
+// return licenses[];
+// if (license === 'MIT')
+//   return '[![License: [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+
+return licenses[license];
+}
+
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenses = {
+    MIT: '[MIT](https://opensource.org/licenses/MIT)',
+    Mozilla: '[Mozilla](https://opensource.org/licenses/MPL-2.0)',
+  IBM: '[IBM](https://opensource.org/licenses/IPL-1.0)',
+    
+  }
+return licenses[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return 'Awesome homework';
+ return `${renderLicenseLink(license)}
+ ${renderLicenseBadge(license)}
+
+
+ `;
+ 
 }
 
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown({title, description, motivation, screenshots, installation, usage,repository, contributors, license}) {
+function generateMarkdown({title, description, motivation, installation, usage, screenshots,repository, contributors, license}) {
   return `# ${title}
-  # Table of Contents
+  ## Table of Contents
   - [Description](#Description)
   - [Motivation](#Motivation)
   - [Installation](#Installation)
   - [Usage](#Usage)
-  - [Screenshots](#Screenshots)
+  - [Video](#Video)
   - [Repository](#Repository)
   - [Contributors](#Contributors)
   - [License](#License)
@@ -29,26 +55,31 @@ function generateMarkdown({title, description, motivation, screenshots, installa
 
 
   ## Description
-    ${motivation}
+    ${description}
+
   ## Motivation
+    ${motivation}
+
+  ## Installation
     ${installation}
 
-  ## ScreenShots
-    ${repository}
-  ## Installation
-    ${contributors}
   ## Usage
-    ${motivation}
+    ${usage}
+
+  ## Video Link
+    ${screenshots}
+
   ## Repository
     ${repository}
 
   ## Contributions
-    ${motivation}
-  ## Credits
-    ${motivation}
+    ${contributors}
+
   ## License
 
   ${renderLicenseSection(license)}
+
+
 
 `;
 }
